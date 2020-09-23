06839f23929d8680c0282a61fd1097c9655ae329
SEOKWEON JUNG <minimus@snu.ac.kr>
2020-09-23 18:23:28 +0900

    force directed so hard

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,77 @@
 import React from "react";
+import * as d3 from 'd3';
+import * as d3Force from 'd3-force';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import Network from "./components/NetworkComponent";
 import Statistics from "./components/StatisticsComponent";
 import Timeline from "./components/TimelineComponent";
 
+import data from './data/filtered_trans.json';
 import "./styles/App.css";
 
-function App() {
+async function readFiles() {
+  let csv = await d3.csv("./data/account.csv").then(function(data) {
+    console.log(data); // [{"Hello": "world"}, …]
+    return data
+  });
+  console.log(csv)
+}
 
+function App() {
+  readFiles();
+  // Data filtering
+  let accountData = []
+  let transData = Object.values(data).map((d: any) => {
+  /*
+  let accountId: string[] = []
+
+
+    if(accountId.indexOf(d.from) >= 0) {
+      let i : number = accountId.indexOf(d.from);
+      accountData[i].balance = +d.from_balance;
+    }
+    else {
+      accountId.push(d.from)
+      accountData.push({
+        id: d.from,
+        balance: +d.from_balance
+      })
+    }
+
+    if(accountId.indexOf(d.to) >= 0) {
+      let i : number = accountId.indexOf(d.to);
+      accountData[i].balance = +d.to_balance;
+    }
+    else {
+      accountId.push(d.to)
+      accountData.push({
+        id: d.to,
+        balance: +d.to_balance
+      })
+    }
+    
+    return {
+      amount: +d.amount,
+      source: d.from,
+      source_balance: +d.from_balance,
+      id: +d.id,
+      k_symbol: d.k_symbol,
+      operation: d.operation,
+      target: d.to,
+      target_balance: d.to_balance,
+      type: d.type,
+    }
+  })
+  */
+  console.log(accountData)
+  console.log(transData)
+  
   return (
     <Container id="container" fluid={true}>
       <Navbar bg="primary" variant="dark" sticky="top">
@@ -22,7 +79,7 @@ function App() {
       </Navbar>
       <Row>
         <Col id='network-cell' lg={9}>
-          <Network></Network>
+          <Network accountData={accountData} transData={transData}></Network>
         </Col>
         <Col id='statistics-cell' lg={3}>
           <Statistics></Statistics>
--- src/components/NetworkComponent.tsx
+++ src/components/NetworkComponent.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
+import * as d3Force from "d3-force";
 
 let svgWidth: number = 1000;
 let svgHeight: number = 800;
@@ -7,10 +8,33 @@ const margin = { left: 40, right: 10, top: 10, bottom: 20 };
 const height = svgHeight - margin.bottom - margin.top;
 const width = svgWidth - margin.left - margin.right;
 
-interface NetworkProps {}
+interface NetNode extends d3Force.SimulationNodeDatum {
+  id: string;
+  balance: number;
+}
+
+interface NetLink extends d3Force.SimulationLinkDatum<NetNode> {
+  amount: number;
+  from: string;
+  from_balance: number | null;
+  id: number;
+  k_symbol: string | null;
+  operation: string | null;
+  to: string;
+  to_balance: string | null;
+  type: string;
+  date: string;
+}
+
+interface NetworkProps {
+  accountData: Account[];
+  transData: Transaction[];
+}
 
 interface NetworkState {
-  isLoaded: boolean;
+  selected: string | undefined;
+  nodes: NetNode[];
+  links: NetLink[];
 }
 
 const translate = (x: number, y: number): string => {
@@ -18,16 +42,11 @@ const translate = (x: number, y: number): string => {
 };
 
 class Network extends Component<NetworkProps, NetworkState> {
-  svg: any;
+  svg: any = React.createRef();
   network: any;
-  xAxis: any;
-  yAxis: any;
-
-  state: NetworkState = {
-    isLoaded: false,
-  };
 
   initNetwork = () => {
+    console.log("init");
     this.network = d3
       .select(this.svg)
       .append("g")
@@ -35,42 +54,73 @@ class Network extends Component<NetworkProps, NetworkState> {
       .attr("width", width)
       .attr("class", "network")
       .attr("transform", translate(margin.left, margin.top));
-
-    this.xAxis = this.network
-      .append("g")
-      .attr("class", "xAxis")
-      .attr("transform", translate(0, height));
-    this.yAxis = this.network.append("g").attr("class", "yAxis");
-
   };
+
   updateNetwork = () => {
-    
+    let forceLink: d3Force.ForceLink<Account, Transaction> = d3Force.forceLink(
+      this.props.transData
+    );
+    let forceType: d3Force.ForceManyBody<Account> = d3.forceManyBody()
+    const simulation = d3
+      .forceSimulation(this.props.accountData)
+      .force(
+        "link",
+        forceLink.id((d) => {
+          return d.id;
+        })
+        .strength((d:Transaction) => {
+          return d.amount/10000
+        })
+      )
+      .force("charge", forceType)
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+    const nodes = this.network
+      .selectAll(".node")
+      .data(this.props.accountData)
+      .join("circle")
+      .attr("class", "node")
+      .attr("r", 2)
+      .attr("fill", "black");
+
+    const links = this.network
+      .selectAll(".link")
+      .data(this.props.transData)
+      .join("line")
+      .attr('stroke', 'black')
+      .attr("stroke-width", 1);
+
+    simulation.on("tick", () => {
+      links
+        .attr("x1", (d: Transaction) => d.source.x)
+        .attr("y1", (d: Transaction) => d.source.y)
+        .attr("x2", (d: Transaction) => d.target.x)
+        .attr("y2", (d: Transaction) => d.target.y);
+
+      nodes.attr("cx", (d: Account) => d.x).attr("cy", (d: Account) => d.y);
+    });
   };
 
   componentDidMount() {
     this.initNetwork();
-    this.setState({
-      isLoaded: true,
-    });
+    this.updateNetwork();
   }
 
-  componentDidUpdate() {}
+  componentDidUpdate() {
+    this.updateNetwork();
+  }
 
   render() {
-    if (this.state.isLoaded) {
-      return (
-        <div style={{ width: "100%", height: "100%", overflow: "scroll" }}>
-          <svg
-            id="network-svg"
-            ref={(el) => (this.svg = el)}
-            height={svgHeight}
-            width={svgWidth}
-          ></svg>
-        </div>
-      );
-    } else {
-      return <p> Loading </p>;
-    }
+    return (
+      <div style={{ width: "100%", height: "100%", overflow: "scroll" }}>
+        <svg
+          id="network-svg"
+          ref={(el) => (this.svg = el)}
+          height={svgHeight}
+          width={svgWidth}
+        ></svg>
+      </div>
+    );
   }
 }
 
