d585e7a46bed49e5dc01fa3279f5c9ba19c6e79c
SEOKWEON JUNG <minimus@snu.ac.kr>
2020-09-24 11:21:38 +0900

    200924

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,202 @@
-import React from "react";
-import * as d3 from 'd3';
-import * as d3Force from 'd3-force';
+import React, { Component } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Navbar from "react-bootstrap/Navbar";
+import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 import Network from "./components/NetworkComponent";
 import Statistics from "./components/StatisticsComponent";
 import Timeline from "./components/TimelineComponent";
 
-import data from './data/filtered_trans.json';
-import "./styles/App.css";
+import transCSV from './data/filtered_real.json';
+import accountCSV from './data/accounts.json';
 
+import "./styles/App.css";
+import { idText } from "typescript";
+
+
+/*
 async function readFiles() {
   let csv = await d3.csv("./data/account.csv").then(function(data) {
     console.log(data); // [{"Hello": "world"}, …]
     return data
   });
   console.log(csv)
+  
+}
+*/
+
+export interface Account {
+  id: string;
+  balance: number | null;
+  date: number | null;
+  frequency: string;
+  district: number;
 }
 
-function App() {
-  readFiles();
-  // Data filtering
-  let accountData = []
-  let transData = Object.values(data).map((d: any) => {
-  /*
-  let accountId: string[] = []
+export interface Transaction {
+  amount: number;
+  from: string;
+  from_balance: number | null;
+  id: number;
+  k_symbol: string | null;
+  operation: string | null;
+  to: string;
+  to_balance: number | null;
+  type: string;
+  date: number;
+}
+interface AppProps {
 
+}
+interface AppState {
+  now: number;
+  accountData: Account[];
+  transData: Transaction[];
+  date: string | undefined;
+}
 
-    if(accountId.indexOf(d.from) >= 0) {
-      let i : number = accountId.indexOf(d.from);
-      accountData[i].balance = +d.from_balance;
-    }
-    else {
-      accountId.push(d.from)
-      accountData.push({
-        id: d.from,
-        balance: +d.from_balance
-      })
-    }
-
-    if(accountId.indexOf(d.to) >= 0) {
-      let i : number = accountId.indexOf(d.to);
-      accountData[i].balance = +d.to_balance;
-    }
-    else {
-      accountId.push(d.to)
-      accountData.push({
-        id: d.to,
-        balance: +d.to_balance
-      })
-    }
-    
+function preprocessTrans(start: number, end: number) {
+  //console.log(start)
+  let transFull: Transaction[] = Object.values(transCSV).map(d => {
+    //console.log('19'+d.date.slice(0, 2) + '-'+d.date.slice(2, 4) + '-' + d.date.slice(4))
     return {
       amount: +d.amount,
-      source: d.from,
-      source_balance: +d.from_balance,
+      from: d.from,
+      from_balance: d.from_balance === null ? null : +d.from_balance!,
       id: +d.id,
       k_symbol: d.k_symbol,
       operation: d.operation,
-      target: d.to,
-      target_balance: d.to_balance,
+      to: d.to,
+      to_balance: d.to_balance === null ? null : +d.to_balance!,
       type: d.type,
+      date: Date.parse('19'+d.date.slice(0, 2) + '-'+d.date.slice(2, 4) + '-' + d.date.slice(4)),
+    }
+  });
+
+  let transSorted = transFull.sort((a: Transaction, b: Transaction) : number => {
+    return - b.date + a.date
+  }).filter(d => (start <= d.date && d.date <= end))
+  return transSorted;
+}
+function preprocessAccount(transData: Transaction[]) {
+  let accountIds: string[] = [];
+  let accountData: Account[] = [];
+  //console.log(accountCSV)
+  let accountFull: Account[] = Object.values(accountCSV).map(d => {
+    return {
+      id: d.id,
+      balance: null,
+      date: d.date === null ? null : Date.parse('19'+d.date!.slice(0, 2) + '-'+d.date!.slice(2, 4) + '-' + d.date!.slice(4)),
+      frequency: d.frequency,
+      district: d.district,
+    }
+  });
+
+  transData.forEach((d:Transaction) => {
+    if(accountIds.includes(d.from)) {
+      let i: number = accountIds.indexOf(d.from);
+      if(d.from_balance != null) {
+        accountData[i].balance = d.from_balance;
+      }
+    }
+    else {
+      accountIds.push(d.from);
+      let acc: Account = accountFull.find(acc => acc.id == d.from)!;
+      if(d.from_balance != null) {
+        acc.balance = d.from_balance;
+      }
+      accountData.push(acc);
+    }
+     
+    if(accountIds.includes(d.to)) {
+      let i: number = accountIds.indexOf(d.to);
+      if(d.to_balance != null) {
+        accountData[i].balance = d.to_balance;
+      }
+    }
+    else {
+      accountIds.push(d.to);
+      let acc: Account = accountFull.find(acc => acc.id == d.to)!;
+      if(d.to_balance != null) {
+        acc.balance = d.to_balance;
+      }
+      accountData.push(acc);
     }
   })
-  */
-  console.log(accountData)
-  console.log(transData)
-  
-  return (
-    <Container id="container" fluid={true}>
-      <Navbar bg="primary" variant="dark" sticky="top">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      </Navbar>
-      <Row>
-        <Col id='network-cell' lg={9}>
-          <Network accountData={accountData} transData={transData}></Network>
-        </Col>
-        <Col id='statistics-cell' lg={3}>
-          <Statistics></Statistics>
-        </Col>
-      </Row>
-      <Row>
-        <Col id='timeline-cell'>
-          <Timeline></Timeline>
-        </Col>
-      </Row>
-    </Container>
-  );
+
+  return accountData
+
 }
+
+
+class App extends Component<AppProps, AppState> {
+  // Data filtering
+  
+
+  constructor(props: AppProps) {
+    super(props);
+    
+    this.state = {
+      now: 0,
+      accountData: [],
+      transData: [],
+      date: '1995-12-01',
+    }
+    this.run = this.run.bind(this)
+  }
+  
+
+  componentDidMount(){
+    console.log('mount')
+    console.log(this.state)
+  }
+
+  run() {
+    console.log(this.state)
+    this.setState({
+      date: this.state.date + '1'
+    })
+  }
+
+
+  render () {
+    let start: number = Date.parse('1995-12-01')
+    let end: number = Date.parse('1995-12-31')
+    let transData: Transaction[] = preprocessTrans(start, end);
+    let accountData: Account[] = preprocessAccount(transData);
+    console.log(this.state)
+    return (
+      <Container id="container" fluid={true}>
+        <Navbar bg="primary" variant="dark" sticky="top">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Button onClick={this.run}>
+            run
+          </Button>
+        </Navbar>
+        <Row>
+          <Col id='network-cell' lg={9}>
+            {this.state.date != undefined ? 
+            <Network accountData={accountData} transData={transData} date={this.state.date}></Network>
+            : <p> loading </p> }
+          </Col>
+          <Col id='statistics-cell' lg={3}>
+            <Statistics></Statistics>
+          </Col>
+        </Row>
+        <Row>
+          <Col id='timeline-cell'>
+            <ProgressBar now={this.state.now} label={`${this.state.now}%` }/>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+  
 
 export default App;
--- src/components/NetworkComponent.tsx
+++ src/components/NetworkComponent.tsx
@@ -1,127 +1,217 @@
-import React, { Component } from "react";
-import * as d3 from "d3";
-import * as d3Force from "d3-force";
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+import * as d3Force from 'd3-force'
+import { Account, Transaction } from '../App'
 
-let svgWidth: number = 1000;
-let svgHeight: number = 800;
-const margin = { left: 40, right: 10, top: 10, bottom: 20 };
-const height = svgHeight - margin.bottom - margin.top;
-const width = svgWidth - margin.left - margin.right;
+let svgWidth: number = 1000
+let svgHeight: number = 800
+const margin = { left: 40, right: 10, top: 10, bottom: 20 }
+const height = svgHeight - margin.bottom - margin.top
+const width = svgWidth - margin.left - margin.right
 
-interface NetNode extends d3Force.SimulationNodeDatum {
-  id: string;
-  balance: number;
-}
-
-interface NetLink extends d3Force.SimulationLinkDatum<NetNode> {
-  amount: number;
-  from: string;
-  from_balance: number | null;
-  id: number;
-  k_symbol: string | null;
-  operation: string | null;
-  to: string;
-  to_balance: string | null;
-  type: string;
-  date: string;
+interface NetNode extends d3Force.SimulationNodeDatum, Account {}
+interface NetLink extends d3Force.SimulationLinkDatum<NetNode>, Transaction {
+    source: string | number | NetNode
+    target: string | number | NetNode
 }
 
 interface NetworkProps {
-  accountData: Account[];
-  transData: Transaction[];
+    accountData: Account[]
+    transData: Transaction[]
+    date: string
 }
 
 interface NetworkState {
-  selected: string | undefined;
-  nodes: NetNode[];
-  links: NetLink[];
+    nodes: NetNode[]
+    links: NetLink[]
+    selected: string[]
 }
 
 const translate = (x: number, y: number): string => {
-  return "translate(" + x + "," + y + ")";
-};
-
-class Network extends Component<NetworkProps, NetworkState> {
-  svg: any = React.createRef();
-  network: any;
-
-  initNetwork = () => {
-    console.log("init");
-    this.network = d3
-      .select(this.svg)
-      .append("g")
-      .attr("height", height)
-      .attr("width", width)
-      .attr("class", "network")
-      .attr("transform", translate(margin.left, margin.top));
-  };
-
-  updateNetwork = () => {
-    let forceLink: d3Force.ForceLink<Account, Transaction> = d3Force.forceLink(
-      this.props.transData
-    );
-    let forceType: d3Force.ForceManyBody<Account> = d3.forceManyBody()
-    const simulation = d3
-      .forceSimulation(this.props.accountData)
-      .force(
-        "link",
-        forceLink.id((d) => {
-          return d.id;
-        })
-        .strength((d:Transaction) => {
-          return d.amount/10000
-        })
-      )
-      .force("charge", forceType)
-      .force("center", d3.forceCenter(width / 2, height / 2));
-
-    const nodes = this.network
-      .selectAll(".node")
-      .data(this.props.accountData)
-      .join("circle")
-      .attr("class", "node")
-      .attr("r", 2)
-      .attr("fill", "black");
-
-    const links = this.network
-      .selectAll(".link")
-      .data(this.props.transData)
-      .join("line")
-      .attr('stroke', 'black')
-      .attr("stroke-width", 1);
-
-    simulation.on("tick", () => {
-      links
-        .attr("x1", (d: Transaction) => d.source.x)
-        .attr("y1", (d: Transaction) => d.source.y)
-        .attr("x2", (d: Transaction) => d.target.x)
-        .attr("y2", (d: Transaction) => d.target.y);
-
-      nodes.attr("cx", (d: Account) => d.x).attr("cy", (d: Account) => d.y);
-    });
-  };
-
-  componentDidMount() {
-    this.initNetwork();
-    this.updateNetwork();
-  }
-
-  componentDidUpdate() {
-    this.updateNetwork();
-  }
-
-  render() {
-    return (
-      <div style={{ width: "100%", height: "100%", overflow: "scroll" }}>
-        <svg
-          id="network-svg"
-          ref={(el) => (this.svg = el)}
-          height={svgHeight}
-          width={svgWidth}
-        ></svg>
-      </div>
-    );
-  }
+    return 'translate(' + x + ',' + y + ')'
 }
 
-export default Network;
+class Network extends Component<NetworkProps, NetworkState> {
+    svg: any = React.createRef()
+    simulation: d3Force.Simulation<NetNode, NetLink> = d3.forceSimulation()
+    network: any
+
+    constructor(props: NetworkProps) {
+        super(props)
+
+        this.state = {
+            nodes: [],
+            links: [],
+            selected: [],
+        }
+    }
+
+    initNetwork = () => {
+        console.log('init')
+        this.network = d3
+            .select(this.svg)
+            .append('g')
+            .attr('height', height)
+            .attr('width', width)
+            .attr('class', 'network')
+            .attr('transform', translate(margin.left, margin.top))
+    }
+
+    updateNetwork = () => {
+        // node data setting
+        console.log('update')
+        const nodes: NetNode[] = this.props.accountData
+        const links: NetLink[] = this.props.transData.map((d) => {
+            return Object.assign(d, { source: d.from, target: d.to })
+        })
+
+        const forceLink: d3Force.ForceLink<
+            NetNode,
+            NetLink
+        > = d3Force.forceLink(links)
+
+        const forceMB: d3Force.ForceManyBody<NetNode> = d3Force.forceManyBody()
+
+        this.simulation = d3Force
+            .forceSimulation(nodes)
+            .force(
+                'link',
+                forceLink.id((d) => d.id).strength(1)
+            )
+            .force('charge', forceMB.strength(-1))
+            //.force("collide", d3.forceCollide())
+            .force('center', d3.forceCenter(width / 2, height / 2))
+        //.force("forceX", d3.forceX())
+        //.force("forceY", d3.forceY());
+        const link = this.network
+            .selectAll('.link')
+            .data(links)
+            .join('line')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+
+        const node = this.network
+            .selectAll('.node')
+            .data(nodes)
+            .join('circle')
+            .attr('r', 4)
+            .attr('fill', 'black')
+            .on('click', (e: MouseEvent, d: NetNode) => {
+                console.log(e)
+                console.log(d)
+                let temp = this.state.selected
+                temp.push(d.id)
+                this.setState({
+                    selected: temp,
+                })
+                this.simulation.restart()
+            })
+            .call(this.drag)
+
+        this.simulation.on('tick', () => {
+            link.attr('x1', (d: any) => {
+                if (this.state.selected.includes(d.from)) {
+                  let i: number = this.state.selected.indexOf(d.from);
+                  return width / (this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.source.x
+                }
+              })
+              .attr('y1', (d: any) => {
+                if (this.state.selected.includes(d.from)) {
+                  let i: number = this.state.selected.indexOf(d.from);
+                    return height / 2 // (this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.source.y
+                }
+              })
+              .attr('x2', (d: any) => {
+                if (this.state.selected.includes(d.to)) {
+                  let i: number = this.state.selected.indexOf(d.to);
+                  return width / (this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.target.x
+                }
+              })
+              .attr('y2', (d: any) => {
+                if (this.state.selected.includes(d.to)) {
+                  let i: number = this.state.selected.indexOf(d.to);
+                  return height / 2 //(this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.target.y
+                }
+              })
+
+            node.attr('cx', (d: any) => {
+                if (this.state.selected.includes(d.id)) {
+                  let i: number = this.state.selected.indexOf(d.id);
+                  return width / (this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.x
+                }
+            }).attr('cy', (d: any) => {
+                if (this.state.selected.includes(d.id)) {
+                  let i: number = this.state.selected.indexOf(d.id);
+                  return height / 2 //(this.state.selected.length+1) * (i+1);
+                } else {
+                    return d.y
+                }
+            })
+        })
+    }
+
+    dragstarted(event: any) {
+        if (!event.active) this.simulation.alphaTarget(0.3).restart()
+        event.subject.fx = event.subject.x
+        event.subject.fy = event.subject.y
+    }
+
+    dragged(event: any) {
+        event.subject.fx = event.x
+        event.subject.fy = event.y
+    }
+
+    dragended(event: any) {
+        if (!event.active) this.simulation.alphaTarget(0)
+        event.subject.fx = null
+        event.subject.fy = null
+    }
+
+    drag = d3
+        .drag()
+        .on('start', (e) => this.dragstarted(e))
+        .on('drag', (e) => this.dragged(e))
+        .on('end', (e) => this.dragended(e))
+
+    handleNodeClick(d: NetNode) {
+        console.log(this)
+        console.log(d)
+
+        //d.fx = width / 2;
+        //d.fy = height / 2;
+    }
+
+    componentDidMount() {
+        this.initNetwork()
+        this.updateNetwork()
+    }
+
+    componentDidUpdate() {
+        //this.updateNetwork()
+    }
+
+    render() {
+        return (
+            <div style={{ width: '100%', height: '100%', overflow: 'scroll' }}>
+                <svg
+                    id="network-svg"
+                    ref={(el) => (this.svg = el)}
+                    height={svgHeight}
+                    width={svgWidth}
+                ></svg>
+            </div>
+        )
+    }
+}
+
+export default Network
